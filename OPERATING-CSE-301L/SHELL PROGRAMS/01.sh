7596cc40a5adeab8180b463469acd629008c1f21
ASHISH-KUMAR-GUPTA-920 <ashishguptaag8351212@gmail.com>
2023-02-28 22:34:34 +0545

    first commit

---
+++ OPERATING-CSE-301L/SHELL PROGRAMS/01.sh
@@ -0,0 +1,30 @@
+# 1. Write a script to find the greatest of three numbers
+#  (numbers passed as command line parameters).
+
+
+Code : 
+echo "Enter first number"
+read n1
+echo "Enter Second number"
+read n2
+echo "Enter Third number"
+read n3
+if [ $n1 -gt $n2 ] && [ $n1 -gt $n3];
+then
+echo"$n1 is the greatest of three numbers"
+elif [ $n2 -gt $n1 ] && [ $n2 -gt $n3];
+then
+echo"$n2 is greatest of three numbers"
+else
+echo "$n3 is greatest of three numbers"
+fi
+
+# Output :
+# Enter first number
+# 12
+# Enter Second number
+# 59
+# Enter Third number
+# 85
+# 85 is greatest of three numbers
+
